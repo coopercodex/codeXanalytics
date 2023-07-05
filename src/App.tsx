ef51199e752c765b17de644c83da715c43188c66
Derek Cooper <derekcooper20@gmail.com>
2023-07-05 15:05:26 -0600

    Adds styling and setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar"
+import Dashboard from "./scenes/dashboard"
+
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), [])
 
   return (
     <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<div>Predics</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,49 @@
+import { useTheme } from "@emotion/react"
+import { Box, Typography } from "@mui/material"
+import FlexBetween from "../FlexBetween"
+import { useState } from "react"
+import { BubbleChart, DonutSmall } from "@mui/icons-material"
+import { Link } from "react-router-dom"
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme()
+  const [selected, setSelected] = useState("dashboard")
+
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      <FlexBetween gap="0.75rem">
+        <BubbleChart sx={{ fontSize: "28px" }} />
+        <Typography variant="h4" fontSize="16px">
+          codeXanalytics
+        </Typography>
+      </FlexBetween>
+
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          ></Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          ></Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  )
+}
+
+export default Navbar
